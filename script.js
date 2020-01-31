73637a71f934596d6b7f54778aa47b6d641008ec
Zack Friendly <friendly.wisconsin@gmail.com>
2020-01-30 19:03:16 -0500

    Built the starting deck with values

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let emptyDeck = []
-let cardNumbers = [2, 3, 4, 5, 6, 7, 8, ]
+let cardDeck = []
+const cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", 'Queen', 'King', 'Ace']
+const cardSuits = ['♠', '♥️', '♣️', '♦️']
+const cardValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+makeDeck = () => {
+ let fullDeck = cardSuits.map(suit => (
+     cardNumbers.map((num, i) => ({
+         card: `${num} ${suit}`,
+         value: cardValues[i]
+        })
+     )
+ )
+ )
+ console.log(fullDeck)
+}
+
+makeDeck()
