cf54a1c821f7e3d620c72fb20fc9f5d72d25424b
Zack Friendly <friendly.wisconsin@gmail.com>
2020-01-30 19:19:07 -0500

    Built the shuffle function and applied it to cardDeck

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let cardDeck = []
 const cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", 'Queen', 'King', 'Ace']
 const cardSuits = ['♠', '♥️', '♣️', '♦️']
-const cardValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 makeDeck = () => {
  let fullDeck = cardSuits.map(suit => (
      cardNumbers.map((num, i) => ({
          card: `${num} ${suit}`,
-         value: cardValues[i]
+         value: i
         })
      )
  )
  )
- console.log(fullDeck)
+return fullDeck[0].concat(fullDeck[1], fullDeck[2], fullDeck[3])
 }
+cardDeck = makeDeck()
 
-makeDeck()
+shuffleDeck = (array) => {
+    var i = 0
+    var j = 0
+    var temp = null
+  
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+   return array
+}
