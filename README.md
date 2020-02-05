6dae7c5e15017cb0cb077133d0cfc86f0fdfe62f
Zack Friendly <friendly.wisconsin@gmail.com>
2020-02-05 11:56:35 -0500

    added README.md

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Game-of-War
-A game of war played through the console
+# Game-of-War Project Requirements
+To play, each player reveals the top card in their stack. The player who played the card with the higher rank (Aces high) takes both cards and puts them at the bottom of their stack in an arbitrary order.
+
+If there is a tie, then it's War! In the card game each player adds places the top three cards of their stack face down, and then each player reveals the top card again. Whoever wins out of the second reveal takes all of the cards, and if there is another tie the process repeats until there is a winner.
+
+## Prerequisites
+
+  * GitHub account
+  * Knowledge of JavaScript for use in console
+  * Understanding of for loops, while loops, and object-building in JavaScript
+
+## My Process
+1. Rubberducked the steps for the game, including:
+   * Making a deck with card numbers, suits, and values
+   * Implementing a shuffle function
+   * Splitting the shuffled deck into two even player decks
+   * Logic for comparing the card values
+   * Pushing the winning cards into the correct deck
+   * Building the spoilsOfWar (tie in card value) logic
+   * Having the game run on a while loop
+   * Ending the game correctly
+2. Wrote the first few functions (createDeck, shuffleDeck, deal)
+3. Built all of the if/else statements to compare card values
+4. Built the spoilsOfWar (tie) function
+5. Used console.logs to debug the game
+6. Built the while loop to have the game run without any input
+7. Added console logs using interpolation to display the cards played and who won each round.
+
+## Deployed with:
+I used GitHub to host my static site, which can be found [**HERE**](https://zfriendly.github.io/Game-of-War.io/).
+
+## Attributions
+Used [THIS](https://www.frankmitchell.org/2015/01/fisher-yates/) code to help understand and build the shuffle cards function (H/T [Frank Mitchell](https://www.frankmitchell.org))
+
+## Contributing/Future Additions
+
+Feel free to find me on [GitHub](https://github.com/zfriendly) if you're interested in contributing! I want to re-write the game using object-oriented programming.
+
+## Screenshots
+
+
+Gameplay function in action:
+![Gameplay](https://i.imgur.com/6ExzeWK.png)
+
+Name Input box (bonus feature):
+![Name Input](https://i.imgur.com/ibuaRQA.png)
+
+## Author
+* [Zack Friendly](https://github.com/zfriendly)
--- script.js
+++ script.js
@@ -1,115 +1,131 @@
-let cardDeck = []
-const cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", 'Queen', 'King', 'Ace']
-const cardSuits = ['♠', '♥️', '♣️', '♦️']
+let cardDeck = [];
+const cardNumbers = [
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  "Jack",
+  "Queen",
+  "King",
+  "Ace"
+];
+const cardSuits = ["♠", "♥️", "♣️", "♦️"];
 
-let userDeck = []
-let computerDeck = []
-let spoilsOfWar = [] 
-// setPlayer = (name) => { 
-//     player1 = name 
+let userDeck = [];
+let computerDeck = [];
+let spoilsOfWar = [];
+// setPlayer = (name) => {
+//     player1 = name
 //     console.log("Welcome " + player1 + ". Type deal() to get started!")
-    
+
 // }
 // console.log("Welcome to War, set your name by typing setPlayer(YOUR NAME in quotes)")
-let player1 = prompt("What is your name?")
+let player1 = prompt("What is your name?");
 
 makeDeck = () => {
- let fullDeck = cardSuits.map(suit => (
-     cardNumbers.map((num, i) => ({
-         card: `${num} ${suit}`,
-         value: i
-        })
-     )
- )
- )
-return fullDeck[0].concat(fullDeck[1], fullDeck[2], fullDeck[3])
-}
-cardDeck = makeDeck()
+  let fullDeck = cardSuits.map(suit =>
+    cardNumbers.map((num, i) => ({
+      card: `${num} ${suit}`,
+      value: i
+    }))
+  );
+  return fullDeck[0].concat(fullDeck[1], fullDeck[2], fullDeck[3]);
+};
+cardDeck = makeDeck();
 
-shuffleDeck = (array) => {
-    var i = 0
-    var j = 0
-    var temp = null
-  
-    for (i = array.length - 1; i > 0; i -= 1) {
-      j = Math.floor(Math.random() * (i + 1))
-      temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
-    }
-   return array
-}
+shuffleDeck = array => {
+  var i = 0;
+  var j = 0;
+  var temp = null;
 
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
 
 deal = () => {
-    const readyDeck = shuffleDeck(cardDeck)
-   userDeck = readyDeck.slice(0, 26).map(i => i)
-   computerDeck = readyDeck.slice(26, readyDeck.length).map(j => j)
-   console.log('Ok! The cards are dealt. Let us begin!')
-}
+  const readyDeck = shuffleDeck(cardDeck);
+  userDeck = readyDeck.slice(0, 26).map(i => i);
+  computerDeck = readyDeck.slice(26, readyDeck.length).map(j => j);
+  console.log("Ok! The cards are dealt. Let us begin!");
+};
 
 playCard = () => {
-    let userCard = userDeck[0]
-    let computerCard = computerDeck[0]
-    if (userDeck.length === 0) {
-        computerDeck.push(...spoilsOfWar)
-        console.log(`${player1} has ${(userDeck.length)} cards. Computer has ${computerDeck.length} cards`)
-        console.log("Game Over!") 
-    }
-    else if (computerDeck.length === 0) {
-        userDeck.push(...spoilsOfWar)
-        console.log(`${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`)
-        console.log("Game Over!")  
-    }
-    else if (userCard.value > computerCard.value) {
-        console.log(`${player1} played ${userCard.card}, Computer played ${computerCard.card}.`)
-        console.log(`${player1} won the hand.`)
-        userDeck.push(computerCard)
-        userDeck.push(userDeck.shift())
-        computerDeck.shift()
-        userDeck.push(...spoilsOfWar)
-        spoilsOfWar = []
-        console.log(`${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`)
-
-        
-    }
-    else if (userCard.value < computerCard.value) {
-        console.log(`${player1} played ${userCard.card}, Computer played ${computerCard.card}.`)
-        console.log('The computer won the hand.')
-        computerDeck.push(userCard)
-        computerDeck.push(computerDeck.shift())
-        userDeck.shift()
-        computerDeck.push(...spoilsOfWar)
-        spoilsOfWar = []
-        console.log(`${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`)
-
-        
-    }
-    else if (userCard.value === computerCard.value) {
-        console.log(`${player1} played ${userCard.card}, Computer played ${computerCard.card}.`)
-        console.log("We have a tie! I.....DE....CLARE.....WAR!")
-       spoilsOfWar.push(userCard)
-       spoilsOfWar.push(computerCard)
-       userDeck.shift()
-       computerDeck.shift()
-       spoilsOfWar.push(...userDeck.slice(0, 3))
-       spoilsOfWar.push(...computerDeck.slice(0, 3))
-       userDeck.shift()
-       userDeck.shift()
-       userDeck.shift()
-       computerDeck.shift()
-       computerDeck.shift()
-       computerDeck.shift()
+  let userCard = userDeck[0];
+  let computerCard = computerDeck[0];
+  if (userDeck.length === 0) {
+    computerDeck.push(...spoilsOfWar);
+    console.log(
+      `${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`
+    );
+    console.log("Game Over!");
+  } else if (computerDeck.length === 0) {
+    userDeck.push(...spoilsOfWar);
+    console.log(
+      `${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`
+    );
+    console.log("Game Over!");
+  } else if (userCard.value > computerCard.value) {
+    console.log(
+      `${player1} played ${userCard.card}, Computer played ${computerCard.card}.`
+    );
+    console.log(`${player1} won the hand.`);
+    userDeck.push(computerCard);
+    userDeck.push(userDeck.shift());
+    computerDeck.shift();
+    userDeck.push(...spoilsOfWar);
+    spoilsOfWar = [];
+    console.log(
+      `${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`
+    );
+  } else if (userCard.value < computerCard.value) {
+    console.log(
+      `${player1} played ${userCard.card}, Computer played ${computerCard.card}.`
+    );
+    console.log("The computer won the hand.");
+    computerDeck.push(userCard);
+    computerDeck.push(computerDeck.shift());
+    userDeck.shift();
+    computerDeck.push(...spoilsOfWar);
+    spoilsOfWar = [];
+    console.log(
+      `${player1} has ${userDeck.length} cards. Computer has ${computerDeck.length} cards`
+    );
+  } else if (userCard.value === computerCard.value) {
+    console.log(
+      `${player1} played ${userCard.card}, Computer played ${computerCard.card}.`
+    );
+    console.log("We have a tie! I.....DE....CLARE.....WAR!");
+    spoilsOfWar.push(userCard);
+    spoilsOfWar.push(computerCard);
+    userDeck.shift();
+    computerDeck.shift();
+    spoilsOfWar.push(...userDeck.slice(0, 3));
+    spoilsOfWar.push(...computerDeck.slice(0, 3));
+    userDeck.shift();
+    userDeck.shift();
+    userDeck.shift();
+    computerDeck.shift();
+    computerDeck.shift();
+    computerDeck.shift();
     //    console.log(userDeck)
     //    console.log(computerDeck)
     //    console.log(spoilsOfWar)
-       playCard();
-}
-
-}
-makeDeck()
-shuffleDeck(cardDeck)
-deal()
+    playCard();
+  }
+};
+makeDeck();
+shuffleDeck(cardDeck);
+deal();
 while (userDeck.length > 0 && computerDeck.length > 0) {
-    playCard()
+  playCard();
 }
